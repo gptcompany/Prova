f25bde7cba4edc3b64c547a3e9ae95ef4fa542e0
gawaboga <samuele.morzenti@gmail.com>
2023-12-11 22:58:42 +0000

    added aws ecs support

---
+++ feed/nbbo.py
@@ -0,0 +1,20 @@
+from cryptofeed import FeedHandler
+from cryptofeed.exchanges import Coinbase, Binance, Bitfinex
+# import logging
+# logging.basicConfig(filename='/var/log/nbbo.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+# logger = logging.getLogger(__name__)
+def nbbo_update(symbol, bid, bid_size, ask, ask_size, bid_feed, ask_feed):
+    print(f'Pair: {symbol} Bid Price: {bid:.2f} Bid Size: {bid_size:.6f} Bid Feed: {bid_feed} Ask Price: {ask:.2f} Ask Size: {ask_size:.6f} Ask Feed: {ask_feed}')
+
+
+def main():
+    path_to_config = '/config_cf.yaml'
+    f = FeedHandler(config=path_to_config)  
+    #symbols = fh.config.config['bf_symbols']
+    symbols = ['BTC-USDT']
+    f.add_nbbo([Coinbase, Binance, Bitfinex], symbols, nbbo_update)
+    f.run()
+    
+if __name__ == '__main__':
+    main()
+    
