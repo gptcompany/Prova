abd4f74eb62f25e8ffa21c1c3205106d98473332
gawaboga <samuele.morzenti@gmail.com>
2023-12-15 17:32:46 +0000

    test redis

--- feed/tests/test_redis.py
+++ feed/tests/test_redis.py
@@ -13,7 +13,7 @@ def add_and_check_key(redis_host, redis_port, key, value):
     """
     try:
         # Connect to Redis
-        r = redis.Redis(host=redis_host, port=redis_port, ssl=ssl_enabled)
+        r = redis.Redis(host=redis_host, port=redis_port, decode_respose=True, ssl=ssl_enabled)
 
         # Add key-value pair
         r.set(key, value)
@@ -22,8 +22,8 @@ def add_and_check_key(redis_host, redis_port, key, value):
         retrieved_value = r.get(key)
 
         # Check if the retrieved value matches the original value
-        if retrieved_value and retrieved_value.decode('utf-8') == value:
-            print(f"Success: The value for '{key}' is persisted as '{retrieved_value.decode('utf-8')}'")
+        if retrieved_value == value:
+            print(f"Success: The value for '{key}' is persisted as '{retrieved_value}'")
         else:
             print(f"Failed: The value for '{key}' did not persist or is incorrect.")
 
@@ -53,7 +53,7 @@ async def check_last_update(redis_host, redis_port, exchanges, symbols):
     """
     try:
         # Connect to Redis
-        r = redis.Redis(host=redis_host, port=redis_port, ssl=ssl_enabled)
+        r = redis.Redis(host=redis_host, port=redis_port, ssl=ssl_enabled, decode_responses=True)
 
         while True:
             current_time = datetime.now(timezone.utc)
