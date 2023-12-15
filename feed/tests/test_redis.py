614c6ad63fd29e17c02fa2b1fff0c8f8483369fb
gawaboga <samuele.morzenti@gmail.com>
2023-12-15 02:49:20 +0000

    test redis

--- feed/tests/test_redis.py
+++ feed/tests/test_redis.py
@@ -22,7 +22,7 @@ async def check_last_update(redis_host, redis_port, exchanges, symbols):
 
             for exchange in exchanges:
                 for symbol in symbols:
-                    key_pattern = f"exchange:{exchange}:{symbol}:book"
+                    key_pattern = f"{exchange}:{symbol}:book"
                     
                     # Retrieve the latest entry's score (timestamp)
                     last_update_score = r.zrange(key_pattern, -1, -1, withscores=True)
