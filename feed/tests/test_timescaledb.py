08a7dd01b243bf666dda2753c9f8b20e37e48518
gawaboga <samuele.morzenti@gmail.com>
2023-12-28 17:08:15 +0000

    update

--- feed/tests/test_timescaledb.py
+++ feed/tests/test_timescaledb.py
@@ -1,5 +1,7 @@
 import asyncio
 import asyncpg
+from datetime import datetime, timezone
+import time
 import yaml
 with open('/config_cf.yaml', 'r') as file:
     config = yaml.safe_load(file)
@@ -22,18 +24,34 @@ async def fetch_latest_receipts(table, user, pw, db, host, port):
         """)
         return result
     finally:
-        await conn.close()
+        await conn.aclose()
+
+def utc_to_local(utc_dt):
+    # Get the local timezone offset
+    local_timezone = datetime.now(timezone.utc).astimezone().tzinfo
+    local_dt = utc_dt.replace(tzinfo=timezone.utc).astimezone(local_timezone)
+    return local_dt
+
+def format_datetime(dt):
+    # Format the datetime object to a string
+    return dt.strftime("%Y-%m-%d %H:%M:%S %Z")
 
 async def main():
     trades_receipts = await fetch_latest_receipts('trades', **postgres_cfg)
     print("Latest Receipts in Trades Table:")
     for record in trades_receipts:
-        print(record)
+        utc_dt = record['latest_receipt']
+        local_dt = utc_to_local(utc_dt)
+        formatted_dt = format_datetime(local_dt)
+        print(f"Symbol: {record['symbol']}, Latest Receipt: {formatted_dt}")
 
     book_receipts = await fetch_latest_receipts('book', **postgres_cfg)
     print("\nLatest Receipts in Book Table:")
     for record in book_receipts:
-        print(record)
+        utc_dt = record['latest_receipt']
+        local_dt = utc_to_local(utc_dt)
+        formatted_dt = format_datetime(local_dt)
+        print(f"Symbol: {record['symbol']}, Latest Receipt: {formatted_dt}")
         
         
         
