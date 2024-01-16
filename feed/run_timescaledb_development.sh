36c205755edd72e52ca4cd44b08c153cecf3a9ea
gawaboga <gawaboga@github.com>
2024-01-16 17:21:26 +0000

    pg scripts

--- feed/run_timescaledb_development.sh
+++ feed/run_timescaledb_development.sh
@@ -10,10 +10,28 @@ PGPASSWORD=$(python3 -c "import yaml; print(yaml.safe_load(open('/config_cf.yaml
 export PGUSER PGHOST PGPORT PGPASSWORD
 CONTAINER_NAME="timescaledb"
 PROD_DB_HOST="57.181.106.64"  # Production Database IP
-
+# AWS S3 settings
+S3_BUCKET="s3://timescalebackups"
+# Logging settings
+HOME="/home/sam"
+LOG_FILE="$HOME/ts_replica.log"
+IP_FILE="ip_development.txt"
+IP_FILE_FOLDER="/home/sam/ip_address/"
 # Function to log messages
+exec 3>>$LOG_FILE
+# Function to log messages and command output to the log file
 log_message() {
-    echo "$(date +"%Y-%m-%d %T"): $1" >&2 
+    local message="$(date +"%Y-%m-%d %T"): $1"
+    echo "$message" >&3  # Log to the log file via fd3
+    echo "$message" >&2  # Display on the screen (stderr)
+    if [ -n "$2" ]; then
+        echo "$2" >&3   # Log stdout to the log file via fd3
+        echo "$2" >&2   # Display stdout on the screen (stderr)
+    fi
+    if [ -n "$3" ]; then
+        echo "$3" >&3   # Log stderr to the log file via fd3
+        echo "$3" >&2   # Display stderr on the screen (stderr)
+    fi
 }
 # Improved error handling within functions
 handle_error() {
@@ -118,7 +136,70 @@ initialize_replication_data() {
         handle_error "Failed to initialize replication data"
     fi
 }
+install_aws_cli() {
+    log_message "Checking for AWS CLI installation..."
+    if ! command -v aws &> /dev/null; then
+        log_message "AWS CLI not found. Starting installation..."
 
+        # Assuming a Debian/Ubuntu-based system. Modify as needed for other distributions.
+        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+        unzip awscliv2.zip
+        sudo ./aws/install
+        if command -v aws &> /dev/null; then
+            log_message "AWS CLI successfully installed."
+        else
+            handle_error "AWS CLI installation failed."
+        fi
+    else
+        log_message "AWS CLI is already installed."
+    fi
+}
+upload_to_s3() {
+    local file_to_upload=$IP_FILE
+
+    # Ensure AWS CLI is installed
+    install_aws_cli
+
+    # Check if the file exists
+    if [ ! -f "$file_to_upload" ]; then
+        log_message "File to upload does not exist: $file_to_upload"
+        return 1
+    fi
+
+    # Upload to S3
+    if aws s3 cp "$file_to_upload" "$S3_BUCKET/"
+    then
+        log_message "Successfully uploaded $file_to_upload to S3 bucket $S3_BUCKET"
+    else
+        log_message "Failed to upload $file_to_upload to S3"
+        return 1
+    fi
+}
+get_public_ip() {
+    # Primary service to fetch the public IP address
+    public_ip=$(curl -s https://ipinfo.io/ip)
+
+    # Check if the IP address is valid; if not, try a secondary service
+    if ! [[ $public_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ || $public_ip =~ ^[0-9a-fA-F:]+$ ]]; then
+        log_message "Primary IP service failed. Trying secondary service..."
+        public_ip=$(curl -s https://ifconfig.me)
+    fi
+
+    # Final check if the IP address is valid
+    if [[ $public_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ || $public_ip =~ ^[0-9a-fA-F:]+$ ]]; then
+        local ip_file_dir="$IP_FILE_FOLDER"
+        mkdir -p "$ip_file_dir"  # Create the directory if it does not exist
+        local ip_file_path="${ip_file_dir}$IP_FILE"
+
+        log_message "$public_ip" > "$ip_file_path"
+        log_message "Public IP address: $public_ip"
+        log_message "Public IP address saved to $ip_file_path"
+    else
+        handle_error "Failed to retrieve a valid public IP address."
+    fi
+}
 # Main script execution
+retry_command get_public_ip 2
+retry_command upload_to_s3 2
 retry_command start_container 3
-initialize_replication_data 1
+retry_command initialize_replication_data 1
