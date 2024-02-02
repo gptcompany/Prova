b39921865c5ce30864886bee8e1f807808274bc3
EC2 Default User <ec2-user@ip-172-31-41-243.ap-northeast-1.compute.internal>
2024-02-02 01:13:50 +0000

    update

--- scripts/install_cc_instance.sh
+++ scripts/install_cc_instance.sh
@@ -10,6 +10,7 @@ if aws --version &>/dev/null; then
     if [ "$(printf '%s\n' "$REQUIRED_VERSION" "$CURRENT_VERSION" | sort -V | head -n1)" != "$REQUIRED_VERSION" ]; then
         echo "Upgrading AWS CLI..."
         # Commands to upgrade AWS CLI
+        sudo apt-get install unzip -y
         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
         unzip -o awscliv2.zip
         sudo ./aws/install --update
@@ -28,7 +29,7 @@ if aws configure list; then
     echo "AWS is configured."
 else
     echo "AWS is not configured."
-    sudo aws configure
+    aws configure
 fi
 # DISABLE REMOVE VOLUME ON TERMINATION
 InstanceId=$(aws ec2 describe-instances --filters "Name=key-name,Values=$INSTANCE_NAME" --query "Reservations[].Instances[].[InstanceId]" --output text)
@@ -55,7 +56,7 @@ sudo swapon /swapfile
 echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
 sudo swapon --show
 # INSTALL PACKAGES AND TERMINAL (USING THE SCRIPT)
-sudo chmod +x $HOME/startarb/scripts/install_packages_cc_instance.sh
+sudo chmod +x $HOME/statarb/scripts/install_packages_cc_instance.sh
 $HOME/startarb/scripts/install_packages_cc_instance.sh
 
 # function to copy from s3 the config files after confirmation of the user
@@ -63,7 +64,7 @@ echo "This will recover standby settings from S3. Do you want to continue? (y/n)
 read -r confirmation
 
 if [[ $confirmation == "y" || $confirmation == "Y" ]]; then
-    sudo chmod +x $HOME/startarb/scripts/recover_standby_settings.sh
+    sudo chmod +x $HOME/statarb/scripts/recover_standby_settings.sh
     $HOME/startarb/scripts/recover_standby_settings.sh
 else
     echo "Recovery settings process aborted."
