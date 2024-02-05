308041376984631851199cc08be44a3b5c07442d
EC2 Default User <ec2-user@ip-172-31-38-68.ap-northeast-1.compute.internal>
2024-02-05 23:46:07 +0000

    update

--- scripts/test_ansible.sh
+++ scripts/test_ansible.sh
@@ -167,20 +167,32 @@ cat <<EOF > $HOME/configure_ssh_from_cc.yml
     ansible_ssh_private_key_file: "{{ lookup('env','HOME') }}/retrieved_key.pem"
     ansible_ssh_common_args: '-o StrictHostKeyChecking=no'
   tasks:
-    - name: Fetch Barman's public SSH key
-      slurp:
-        src: "/home/barman/.ssh/id_rsa.pub"
+    - name: "Fetch Barman's public SSH key"
+      command: "cat /home/barman/.ssh/id_rsa.pub"
       register: barman_ssh_pub_key
-      delegate_to: localhost
+      delegate_to: "localhost"
       become: true
-      become_user: barman
+      become_user: "barman"
+  # tasks:
+  #   - name: Fetch Barman's public SSH key
+  #     slurp:
+  #       src: "/home/barman/.ssh/id_rsa.pub"
+  #     register: barman_ssh_pub_key
+  #     delegate_to: localhost
+  #     become: true
+  #     become_user: barman
 
-    - name: Ensure Postgres user can SSH into each server without a password
-      authorized_key:
-        user: postgres
-        state: present
-        key: "{{ barman_ssh_pub_key.content | b64decode }}"
-      become: true  # You might need to adjust this depending on the permissions of the ansible user
+    - name: "Ensure Postgres user can SSH into each server without a password"
+      shell: "echo '{{ barman_ssh_pub_key.stdout }}' >> ~postgres/.ssh/authorized_keys"
+      become: true
+      become_user: "postgres"
+
+    # - name: Ensure Postgres user can SSH into each server without a password
+    #   authorized_key:
+    #     user: postgres
+    #     state: present
+    #     key: "{{ barman_ssh_pub_key.content | b64decode }}"
+    #   become: true  # You might need to adjust this depending on the permissions of the ansible user
 
 EOF
 
