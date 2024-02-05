78c6afb54249b86bd5192a10876d6e9106b13deb
EC2 Default User <ec2-user@ip-172-31-38-68.ap-northeast-1.compute.internal>
2024-02-05 22:48:02 +0000

    update

--- scripts/test_ansible.sh
+++ scripts/test_ansible.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Check for the correct number of arguments
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 TIMESCALEDB_VPC_IP"
-    exit 1
-fi
+# if [ "$#" -ne 1 ]; then
+#     echo "Usage: $0 TIMESCALEDB_PRIVATE_IP"
+#     exit 1
+# fi
 
 # Assign arguments to variables
-TIMESCALEDB_VPC_IP=$1
+TIMESCALEDB_PRIVATE_IP="172.31.35.73"
+TIMESCALEDB_PUBLIC_IP="57.181.106.64"
+CLUSTERCONTROL_PRIVATE_IP="172.31.32.75"
+CLUSTERCONTROL_PUBLIC_IP="43.207.147.235"
+STANDBY_PUBLIC_IP="timescaledb.mywire.org"
+ECS_INSTANCE_PRIVATE_IP="172.31.38.68"
+ECS_INSTANCE_PUBLIC_IP="52.193.34.34"
 AWS_SECRET_ID="ultimaec2key"
 
 # Check if Ansible is installed, install if not
@@ -36,7 +42,7 @@ cat <<EOF > $HOME/configure_barman_on_cc.yml
   hosts: localhost
   become: yes
   vars:
-    timescaledb_vpc_ip: "$TIMESCALEDB_VPC_IP"
+    timescaledb_PRIVATE_ip: "$TIMESCALEDB_PRIVATE_IP"
 
   tasks:
     - name: Check if barman user exists
@@ -91,13 +97,9 @@ EOF
 # Generate Ansible playbook for SSH setup
 cat <<EOF > $HOME/configure_ssh_from_cc.yml
 ---
-- name: Setup SSH Access for ubuntu User
+- name: Setup SSH Key for ubuntu User Locally
   hosts: localhost
-  become: yes
-  vars:
-    timescaledb_vpc_ip: "$TIMESCALEDB_VPC_IP"
-    retrieved_key_path: "$HOME/retrieved_key.pem"
-
+  gather_facts: no
   tasks:
     - name: Check if SSH public key exists for ubuntu user
       stat:
@@ -111,24 +113,46 @@ cat <<EOF > $HOME/configure_ssh_from_cc.yml
         ssh_key_file: "/home/ubuntu/.ssh/id_rsa"
       when: ssh_pub_key.stat.exists == false
 
-    - name: Fetch the public key
+- name: Setup SSH Access for ubuntu User on TimescaleDB Servers
+  hosts: timescaledb_servers
+  gather_facts: no
+  vars:
+    ansible_user: ubuntu
+    ansible_ssh_private_key_file: "{{ lookup('env','HOME') }}/retrieved_key.pem"
+    ansible_ssh_common_args: '-o StrictHostKeyChecking=no'
+  tasks:
+    - name: Fetch the public key of ubuntu user
       slurp:
-        src: "/home/ubuntu/.ssh/id_rsa.pub"
+        src: "{{ lookup('env','HOME') }}/.ssh/id_rsa.pub"
       register: ubuntu_ssh_pub_key
+      delegate_to: localhost
 
-    - name: Ensure ubuntu user can SSH into TIMESCALEDB_VPC_IP
+    - name: Ensure ubuntu user can SSH into each server without a password
       authorized_key:
         user: ubuntu
         state: present
         key: "{{ ubuntu_ssh_pub_key.content | b64decode }}"
-      delegate_to: "{{ timescaledb_vpc_ip }}"
-      vars:
-        ansible_user: ubuntu
-        ansible_ssh_private_key_file: "{{ retrieved_key_path }}"
+
 EOF
 
 echo "Playbooks created: configure_barman_on_cc.yml and configure_ssh_from_cc.yml"
+
+# Generate Ansible inventory
+cat <<EOF > $HOME/timescaledb_inventory.yml
+---
+all:
+  vars:
+    ansible_user: ubuntu
+    ansible_ssh_private_key_file: "{{ ansible_env.HOME }}/retrieved_key.pem"
+    ansible_ssh_common_args: '-o StrictHostKeyChecking=no'
+  children:
+    timescaledb_servers:
+      hosts:
+        $STANDBY_PUBLIC_IP: {}
+        $TIMESCALEDB_PRIVATE_IP: {}
+EOF
 echo "Proceed with running Ansible playbooks as needed."
 
+
 ansible-playbook $HOME/configure_barman_on_cc.yml
-ansible-playbook $HOME/configure_ssh_from_cc.yml
+ansible-playbook -i $HOME/timescaledb_inventory.yml $HOME/configure_ssh_from_cc.yml
