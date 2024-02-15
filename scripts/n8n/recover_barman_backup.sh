7d720a63df8ab040fbb0c1a05e0d40e93e81d58c
EC2 Default User <ec2-user@ip-172-31-38-68.ap-northeast-1.compute.internal>
2024-02-15 19:27:17 +0000

    update

--- scripts/n8n/recover_barman_backup.sh
+++ scripts/n8n/recover_barman_backup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Define server identifier and remote PostgreSQL instance
-SERVER_ID="timesacledb"
+SERVER_ID="timescaledb"
 REMOTE_HOST="timescaledb.mywire.org"
 #REMOTE_PGDATA_PATH="/var/lib/postgresql/15/main" # Update with actual data directory path on the remote host
 REMOTE_PGDATA_PATH=$(ssh postgres@$REMOTE_HOST "psql -t -c 'SHOW data_directory;'")
 echo "Remote PostgreSQL data directory: $REMOTE_PGDATA_PATH"
 # Get the latest backup ID for the server
 LATEST_BACKUP_ID=$(sudo -i -u barman barman list-backup $SERVER_ID | head -n 1 | awk '{print $2}')
-
+sudo -i -u barman barman list-backup $SERVER_ID
+echo "Latest backup : $LATEST_BACKUP_ID"
 # Check if a backup ID was found
 if [ -z "$LATEST_BACKUP_ID" ]; then
     echo "No backup found for server $SERVER_ID"
