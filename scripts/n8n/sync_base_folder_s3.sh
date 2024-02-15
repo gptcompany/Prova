5b958009e36199f0d7d2a981e2accd7e6c61dc1b
EC2 Default User <ec2-user@ip-172-31-38-68.ap-northeast-1.compute.internal>
2024-02-15 19:37:27 +0000

    update

--- scripts/n8n/sync_base_folder_s3.sh
+++ scripts/n8n/sync_base_folder_s3.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 SERVER="timescaledb"
+# List all failed backups for the server and delete them
+failed_backups=$(sudo -i -u barman barman list-backup $SERVER | grep FAILED | awk '{print $2}')
+for backup_id in $failed_backups; do
+    echo "Deleting failed backup: $backup_id"
+    barman delete $SERVER $backup_id
+done
 # Define local directory and S3 bucket path
 local_directory=$(sudo -i -u barman barman show-server "$SERVER" | grep "basebackups_directory" | awk '{print $2}')
 echo "$local_directory"
