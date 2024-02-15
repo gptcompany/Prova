9ab6309f1c3db4dff7140f642903ee2a39d65ffb
EC2 Default User <ec2-user@ip-172-31-38-68.ap-northeast-1.compute.internal>
2024-02-15 22:28:03 +0000

    update

--- scripts/n8n/sync_base_folder_s3.sh
+++ scripts/n8n/sync_base_folder_s3.sh
@@ -2,14 +2,19 @@
 SERVER="timescaledb"
 # List all failed backups for the server and delete them
 failed_backups=$(sudo -i -u barman barman list-backup $SERVER | grep FAILED | awk '{print $2}')
-echo "failed backups: $failed_backups"
-for backup_id in $failed_backups; do
-    echo "Deleting failed backup: $backup_id"
-    sudo -i -u barman barman delete $SERVER $backup_id
-done
+# Check if there are any failed backups
+if [ -z "$failed_backups" ]; then
+    echo "No failed backups."
+else
+    echo "Failed backups: $failed_backups"
+    for backup_id in $failed_backups; do
+        echo "Deleting failed backup: $backup_id"
+        sudo -i -u barman barman delete $SERVER $backup_id
+    done
+fi
 # Define local directory and S3 bucket path
 local_directory=$(sudo -i -u barman barman show-server "$SERVER" | grep "basebackups_directory" | awk '{print $2}')
-echo "$local_directory"
+echo "Local directory: $local_directory"
 s3_bucket="s3://timescalebackups/$SERVER/base"
 
 # Sync local directory to S3 bucket, including deletion of files not present locally
