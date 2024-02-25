c31d360beaa0967c37172ab30b4cf207f5e7b338
EC2 Default User <ec2-user@ip-172-31-38-68.ap-northeast-1.compute.internal>
2024-02-25 20:04:51 +0000

    update

--- scripts/n8n/migrate_db_csv.sh
+++ scripts/n8n/migrate_db_csv.sh
@@ -8,61 +8,71 @@ PGPORT_DEST="5433"
 TABLES=("trades" "book" "open_interest" "funding" "liquidations")
 TSDBADMIN="tsdbadmin"
 TIMESCALEDBPASSWORD=$(aws ssm get-parameter --name timescaledbpassword --with-decryption --query 'Parameter.Value' --output text)
-export SOURCE=postgres://postgres:$TIMESCALEDBPASSWORD@localhost:$PGPORT_SRC/$DB_NAME
-export TARGET=postgres://postgres:$TIMESCALEDBPASSWORD@localhost:$PGPORT_DEST/$DB_NAME
+SOURCE=postgres://postgres:$TIMESCALEDBPASSWORD@localhost:$PGPORT_SRC/$DB_NAME
+TARGET=postgres://postgres:$TIMESCALEDBPASSWORD@localhost:$PGPORT_DEST/$DB_NAME
 
-# Function to execute a command as postgres user on the remote host
+# Execute a command as postgres user on the remote host
 execute_as_postgres() {
     ssh -T postgres@$REMOTE_HOST "PGPASSWORD='$TIMESCALEDBPASSWORD' $1"
 }
 
-# Function to check if a table exists and then convert it to a hypertable
-check_and_create_hypertable() {
-    local table_name=$1
-    local time_column_name=$2
-
-    # Check if the table exists
-    local check_exists_cmd="psql -p $PGPORT_DEST -d $DB_NAME -c \"SELECT to_regclass('public.$table_name');\""
-    local table_exists=$(execute_as_postgres "$check_exists_cmd" | grep -v to_regclass | grep -v row | grep -v -- '---' | grep -v '(' | tr -d '[:space:]')
-
-    # If the table exists, convert it to a hypertable
-    if [ "$table_exists" = "public.$table_name" ]; then
-        echo "Table $table_name exists. Converting to hypertable..."
-        local hypertable_cmd="psql -p $PGPORT_DEST -d $DB_NAME -c \"SELECT create_hypertable('$table_name', '$time_column_name', if_not_exists => TRUE, chunk_time_interval => INTERVAL '10 minutes');\""
-        execute_as_postgres "$hypertable_cmd"
-    else
-        echo "Table $table_name does not exist. Skipping hypertable conversion."
-    fi
-}
-
-# Function to remove retention policy if it exists
-remove_retention_policy_if_exists() {
-    local table_name=$1
-    echo "Checking and removing retention policy for $table_name if exists..."
-    local remove_policy_cmd="psql -p $PGPORT_DEST -d $DB_NAME -c \"SELECT remove_retention_policy('public.$table_name', if_exists => TRUE);\""
-    execute_as_postgres "$remove_policy_cmd"
-}
-
-echo "Dump the database roles from the source database"
-execute_as_postgres 'pg_dumpall -d "'"$SOURCE"'" -l '"$DB_NAME"' --quote-all-identifiers --roles-only --file=roles.sql'
+# Dump and restore schema
+echo "Dumping the database roles from the source database"
+execute_as_postgres "pg_dumpall -d '$SOURCE' -l '$DB_NAME' --quote-all-identifiers --roles-only --file=roles.sql"
 
 echo "Migrating schema pre-data"
 execute_as_postgres "PGPASSWORD='$TIMESCALEDBPASSWORD' pg_dump -U postgres -h localhost -p $PGPORT_SRC -Fc -v --section=pre-data --exclude-schema='_timescaledb*' -f dump_pre_data.dump $DB_NAME"
-echo "Restoring the dump pre data"
+
+echo "Restoring the dump pre-data"
 execute_as_postgres "PGPASSWORD='$TIMESCALEDBPASSWORD' pg_restore -U postgres -h localhost -p $PGPORT_DEST --no-owner -Fc -v -d $DB_NAME dump_pre_data.dump"
 
-# Loop through each table to convert to hypertable and handle retention policy
+# Convert tables to hypertables
 for TABLE_NAME in "${TABLES[@]}"; do
-    # Determine the correct time column name
+    echo "Processing table: $TABLE_NAME"
+    
+    # Define time column name based on the table
     TIME_COLUMN_NAME="timestamp"
     if [ "$TABLE_NAME" = "book" ]; then
         TIME_COLUMN_NAME="receipt"
     fi
+    
+    # Check if the table exists before converting to hypertable
+    echo "Checking if $TABLE_NAME exists"
+    execute_as_postgres "psql -d $DB_NAME -c \"SELECT to_regclass('public.$TABLE_NAME');\""
 
-    # Check if the table exists and convert it to a hypertable
-    check_and_create_hypertable "$TABLE_NAME" "$TIME_COLUMN_NAME"
-    # Remove retention policy if it exists
-    remove_retention_policy_if_exists "$TABLE_NAME"
+    # Convert table to hypertable
+    echo "Converting $TABLE_NAME to hypertable"
+    execute_as_postgres "psql -d $DB_NAME -c \"SELECT create_hypertable('$TABLE_NAME', '$TIME_COLUMN_NAME', if_not_exists => TRUE, chunk_time_interval => INTERVAL '10 minutes');\""
+done
+
+# Data export, import, and ensure compression
+echo "Looping through each table to export, import data, and ensure compression"
+for TABLE_NAME in "${TABLES[@]}"; do
+    FILE_NAME="${TABLE_NAME}.csv"
+    
+    # Export data from source table to CSV
+    execute_as_postgres "psql -d $DB_NAME -c \"\copy (SELECT * FROM $TABLE_NAME) TO '$FILE_NAME' WITH (FORMAT CSV);\""
+
+    # Check and remove retention policy if it exists
+    echo "Checking and removing retention policy for $TABLE_NAME if exists..."
+    remove_policy_cmd="psql -p $PGPORT_DEST -d $DB_NAME -c \"SELECT remove_retention_policy('public.$TABLE_NAME', if_exists => TRUE);\""
+    execute_as_postgres "$remove_policy_cmd"
+
+    # Import data from CSV into target table
+    echo "Importing data into $TABLE_NAME"
+    execute_as_postgres "psql -d $DB_NAME -c \"\copy $TABLE_NAME FROM '$FILE_NAME' WITH (FORMAT CSV);\""
+
+    # Ensure compression is enabled for the hypertable
+    echo "Ensuring compression is enabled for $TABLE_NAME"
+    compression_cmd="psql -p $PGPORT_DEST -d $DB_NAME -c \"ALTER TABLE $TABLE_NAME SET (timescaledb.compress, timescaledb.compress_segmentby = 'YOUR_COLUMN'); SELECT add_compression_policy('$TABLE_NAME', INTERVAL '7 days', if_not_exists => TRUE);\""
+    execute_as_postgres "$compression_cmd"
+    
+    # Cleanup: remove the CSV file
+    execute_as_postgres "rm ~/$FILE_NAME"
 done
 
 echo "Data migration completed."
+
+
+
+
