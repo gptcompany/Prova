48bfcacbf1aaea8c6231cec241371987aa366c49
EC2 Default User <ec2-user@ip-172-31-38-68.ap-northeast-1.compute.internal>
2024-02-27 01:14:09 +0000

    update

--- scripts/n8n/migrate_db_csv.sh
+++ scripts/n8n/migrate_db_csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Redirect stdout and stderr to a log file and also echo it
-exec > >(tee -a ~/migrate_db.log) 2>&1
+# exec > >(tee -a ~/migrate_db.log) 2>&1
 
 # Configuration
 REMOTE_HOST=$(aws ssm get-parameter --name STANDBY_PUBLIC_IP --with-decryption --query 'Parameter.Value' --output text)
@@ -28,7 +28,7 @@ retry_command() {
 
     while [ $attempts -lt $max_attempts ]; do
         attempts=$((attempts+1))
-        echo "Attempt $attempts"
+        # echo "Attempt $attempts"
         
         if output=$(execute_as_postgres "$command" 2>&1); then
             echo "Command succeeded [OKAY]."
@@ -55,14 +55,14 @@ retry_command() {
 }
 echo "Identifying and removing existing retention policies in the source DB"
 for TABLE_NAME in "${TABLES[@]}"; do
-    echo "Processing retention policy for table: $TABLE_NAME"
+    echo "Processing retention policy for table: $TABLE_NAME on source"
     
     # Identify existing retention policies for the table
-    echo "Identifying existing retention policies for $TABLE_NAME"
+    echo "Identifying existing retention policies for $TABLE_NAME on source"
     retry_command "psql -p $PGPORT_SRC -d $DB_NAME -c \"SELECT * FROM timescaledb_information.jobs WHERE proc_name = 'policy_retention' AND hypertable_name = '$TABLE_NAME';\""
     
     # Remove the retention policy if exists
-    echo "Removing retention policy for $TABLE_NAME if exists"
+    echo "Removing retention policy for $TABLE_NAME if exists on source"
     retry_command "psql -p $PGPORT_SRC -d $DB_NAME -c \"SELECT remove_retention_policy('$TABLE_NAME', if_exists => TRUE);\""
 done
 
@@ -70,10 +70,10 @@ done
 echo "Dumping the database roles from the source database"
 retry_command "pg_dumpall -p $PGPORT_SRC -d '$SOURCE' -l '$DB_NAME' --quote-all-identifiers --roles-only --file=roles.sql"
 
-echo "Migrating schema pre-data"
+echo "Migrating schema pre-data on source"
 retry_command "PGPASSWORD='$TIMESCALEDBPASSWORD' pg_dump -U postgres -h localhost -p $PGPORT_SRC -Fc -v --section=pre-data --exclude-schema='_timescaledb*' -f dump_pre_data.dump $DB_NAME"
 
-echo "Restoring the dump pre-data"
+echo "Restoring the dump pre-data on target"
 retry_command "PGPASSWORD='$TIMESCALEDBPASSWORD' pg_restore -U postgres -h localhost -p $PGPORT_DEST --no-owner -Fc -v -d $DB_NAME dump_pre_data.dump"
 
 # Convert tables to hypertables in target db
@@ -87,11 +87,11 @@ for TABLE_NAME in "${TABLES[@]}"; do
     fi
     
     # Check if the table exists before converting to hypertable
-    echo "Checking if $TABLE_NAME exists"
+    echo "Checking if $TABLE_NAME exists on target"
     retry_command "psql -p $PGPORT_DEST -d $DB_NAME -c \"SELECT to_regclass('public.$TABLE_NAME');\""
 
     # Convert table to hypertable
-    echo "Converting $TABLE_NAME to hypertable"
+    echo "Converting $TABLE_NAME to hypertable on target"
     retry_command "psql -p $PGPORT_DEST -d $DB_NAME -c \"SELECT create_hypertable('$TABLE_NAME', '$TIME_COLUMN_NAME', if_not_exists => TRUE, chunk_time_interval => INTERVAL '10 minutes');\""
 done
 
@@ -101,17 +101,16 @@ for TABLE_NAME in "${TABLES[@]}"; do
     FILE_NAME="${TABLE_NAME}.csv"
     # Export data from source table to CSV
     # execute_as_postgres "psql -d $DB_NAME -c \"\copy (SELECT * FROM $TABLE_NAME) TO '$FILE_NAME' WITH (FORMAT CSV);\""
+    echo "Copy data for $TABLE_NAME from source"
     psql_copy_cmd1="psql -p $PGPORT_SRC -d $DB_NAME -c \"\copy (SELECT * FROM $TABLE_NAME) TO '/var/lib/postgresql/$FILE_NAME' WITH (FORMAT CSV);\""
     retry_command "$psql_copy_cmd1"
     # Check and remove retention policy if it exists
-    echo "Checking and removing retention policy for $TABLE_NAME if exists..."
+    echo "Checking and removing retention policy for $TABLE_NAME if exists on target"
     remove_policy_cmd="psql -p $PGPORT_DEST -d $DB_NAME -c \"SELECT remove_retention_policy('public.$TABLE_NAME', if_exists => TRUE);\""
     retry_command "$remove_policy_cmd"
 
     # Import data from CSV into target table
-    echo "Importing data into $TABLE_NAME"
-    # execute_as_postgres "psql -d $DB_NAME -c \"\copy $TABLE_NAME FROM '$FILE_NAME' WITH (FORMAT CSV);\""
-    # Create temporary table
+    echo "Importing data into $TABLE_NAME on target"
     # Insert data from temporary table into target table, skipping duplicates
     psql_copy_cmd="psql -p $PGPORT_DEST -d $DB_NAME -c \"
     BEGIN;
@@ -122,7 +121,6 @@ for TABLE_NAME in "${TABLES[@]}"; do
     COMMIT;
     \""
     
-    # Call retry_command function with the constructed psql copy command
     retry_command "$psql_copy_cmd"
 
     echo "Cleanup: remove the CSV file"
